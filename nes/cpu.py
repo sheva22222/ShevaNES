a2e1f942044f6bde86f52e09efd90b3690ed3b4f
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 17:21:32 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -23,6 +23,6 @@ class CPU:
         else:
             raise Exception(f"Unknown opcode {hex(opcode)}")
             
-        elif opcode == 0xE8:  # INX
-              self.X = (self.X + 1) & 0xFF
+    elif opcode == 0xE8:  # INX
+         self.X = (self.X + 1) & 0xFF
     
