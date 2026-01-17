2f8132f116d11d47a6647c52e475ac234d26cfe8
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 23:48:33 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -6,7 +6,7 @@ program = [
     0x4C, 0x05, 0x00,  # JMP $0005
     0xA9, 0x01,        # LDA #$01 (пропуск)
     0xA9, 0x05,        # LDA #$05
-    0x00               # BRK
+    0x00
 ]
 
 cpu.load_program(program)
