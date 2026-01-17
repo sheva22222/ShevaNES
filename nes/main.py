f4ead758792a737cb3b130a166ae3cdce5e4bb25
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 17:54:54 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,10 +3,16 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 0x00,
+    0xA9, 0x00,  # LDA #0 → Z = 1
+    0xF0, 0x02,  # BEQ +2
+    0xA9, 0x01,  # (пропускается)
+    0xA9, 0x05,  # A = 5
 ]
 
 cpu.load_program(program)
-cpu.step()
 
-print(bin(cpu.P))
+for _ in range(4):
+    cpu.step()
+
+print(cpu.A)
+
