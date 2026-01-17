b4af2640c6fd7c6fa57a363d288ba071fd9d9094
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 17:47:54 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,13 +3,10 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 0x2A,  # LDA #42
-    0x85, 0x10,  # STA $10
+    0xA9, 0x00,
 ]
 
 cpu.load_program(program)
-
-cpu.step()
 cpu.step()
 
-print(cpu.memory[0x10])
+print(bin(cpu.P))
