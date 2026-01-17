b3f6872f247473184ba49173524924dfb8c7826f
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 17:59:56 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -25,40 +25,32 @@ class CPU:
            self.P &= 0b01111111
     
     def step(self):
-        opcode = self.memory[self.PC]
+    opcode = self.memory[self.PC]
+    self.PC += 1
+
+    if opcode == 0xA9:  # LDA immediate
+        value = self.memory[self.PC]
+        self.PC += 1
+        self.A = value
+        self.update_zn(self.A)
+
+    elif opcode == 0xE8:  # INX
+        self.X = (self.X + 1) & 0xFF
+        self.update_zn(self.X)
+
+    elif opcode == 0x85:  # STA zeropage
+        addr = self.memory[self.PC]
+        self.PC += 1
+        self.memory[addr] = self.A
+
+    elif opcode == 0xF0:  # BEQ
+        offset = self.memory[self.PC]
         self.PC += 1
 
-        if opcode == 0xA9:  # LDA immediate
-            value = self.memory[self.PC]
-            self.PC += 1
-            self.A = value
-
-        elif opcode == 0xE8:  # INX
-            self.X = (self.X + 1) & 0xFF
-
-        elif opcode == 0x85:  # STA zeropage
-            addr = self.memory[self.PC]
-            self.PC += 1
-            self.memory[addr] = self.A
-
-        if opcode == 0xA9:  # LDA immediate
-          value = self.memory[self.PC]
-          self.PC += 1
-          self.A = value
-          self.update_zn(self.A)
-
-        elif opcode == 0xE8:  # INX
-            self.X = (self.X + 1) & 0xFF
-            self.update_zn(self.X)
-
-        elif opcode == 0xF0:  # BEQ
-            offset = self.memory[self.PC]
-            self.PC += 1
-
         if self.P & 0b00000010:  # Z = 1
-          if offset & 0x80:
-             offset -= 256
-             self.PC += offset
+            if offset & 0x80:
+                offset -= 256
+            self.PC += offset
 
-        else:
-            raise Exception(f"Unknown opcode {hex(opcode)}")
+    else:
+        raise Exception(f"Unknown opcode {hex(opcode)}")
