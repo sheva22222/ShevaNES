8ce2135cfb8cc33d193d3e0ded7cd7571df768d4
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 21:40:45 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -1,5 +1,24 @@
+from cpu import CPU
+
+cpu = CPU()
+
+program = [
+    0xA9, 0x05,  # LDA #5
+    0xC9, 0x05,  # CMP #5
+    0x00
+]
+
+cpu.load_program(program)
+
+try:
+    while True:
+        cpu.step()
+except StopIteration:
+    pass
+
 print(hex(cpu.A))
 print("C =", cpu.P & 1)
 print("Z =", (cpu.P >> 1) & 1)
 print("N =", (cpu.P >> 7) & 1)
 print("P =", bin(cpu.P))
+
