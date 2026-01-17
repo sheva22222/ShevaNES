7e83d177588e7c257eae422036c8fa5d92f96649
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 18:53:27 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,9 +3,10 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA2, 0x05,      # LDX #5
-    0xCA,            # DEX
-    0xD0, 0xFD       # BNE назад на DEX
+    0xA9, 0x2A,        # LDA #42
+    0x8D, 0x00, 0x02,  # STA $0200
+    0xA9, 0x00,        # LDA #0
+    0xA9, 0x2A         # LDA #42
 ]
 
 cpu.load_program(program)
@@ -16,4 +17,5 @@ try:
 except StopIteration:
     pass
 
-print(cpu.X)
+print(cpu.A)
+print(cpu.memory[0x0200])
