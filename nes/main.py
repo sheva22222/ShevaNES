283fe7772a14f7dfe283324f5c9200c9ca89185d
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 21:30:40 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,10 +3,8 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 5,      # LDA #5
-    0xC9, 5,      # CMP #5
-    0xD0, 0x02,   # BNE skip
-    0xA9, 1,      # LDA #1
+    0xA9, 0x05,  # LDA #5
+    0xC9, 0x05,  # CMP #5
     0x00
 ]
 
