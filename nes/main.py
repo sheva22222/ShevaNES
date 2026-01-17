1a8840b4879fdf2e5409e5feee7a3f9e08090cb2
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 17:26:58 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,9 +3,9 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xE8,  # INX
-    0xE8,  # INX
-    0xE8,  # INX
+    0xE8,
+    0xE8,
+    0xE8,
 ]
 
 cpu.load_program(program)
@@ -13,4 +13,4 @@ cpu.load_program(program)
 for _ in range(3):
     cpu.step()
 
-print(cpu.X)  # должно быть 3
+print(cpu.X)
