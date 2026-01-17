9c6071c4b64897528aa249529cc56e97e88ae3ad
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 23:11:21 +0200

    try 2 test

--- nes/main.py
+++ nes/main.py
@@ -3,15 +3,20 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 0xFF,       # LDA #$FF  → N = 1
-    0x30, 0x02,       # BMI +2 (должен сработать)
-    0xA9, 0x01,       # LDA #$01 (пропуск)
-    0xA9, 0x05        # LDA #$05
+    0xA9, 0xFF,  # LDA #$FF
+    0x30, 0x02,  # BMI +2
+    0xA9, 0x01,  # LDA #$01
+    0xA9, 0x05,  # LDA #$05
+    0x00         # BRK
 ]
 
 cpu.load_program(program)
-for _ in range(10):
-    cpu.step()
+
+try:
+    while True:
+        cpu.step()
+except StopIteration:
+    pass
 
 print(hex(cpu.A))
 print("N =", (cpu.P >> 7) & 1)
