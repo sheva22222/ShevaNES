d795f7c58b4f7bc77bb1f75d94a0dd3d556c1ace
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 23:06:49 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -3,9 +3,8 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 0x03,       # LDA #$03
-    0xC9, 0x05,       # CMP #$05  → C = 0
-    0x90, 0x02,       # BCC +2 (должен сработать)
+    0xA9, 0xFF,       # LDA #$FF  → N = 1
+    0x30, 0x02,       # BMI +2 (должен сработать)
     0xA9, 0x01,       # LDA #$01 (пропуск)
     0xA9, 0x05        # LDA #$05
 ]
@@ -15,6 +14,4 @@ for _ in range(10):
     cpu.step()
 
 print(hex(cpu.A))
-print("C =", cpu.P & 1)
-
-
+print("N =", (cpu.P >> 7) & 1)
