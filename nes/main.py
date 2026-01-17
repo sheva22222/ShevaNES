6534757dcd1cb4677094dc70391dff9a8de913b3
makaherpasswordlostmedia <emssaoig1@gmail.com>
2026-01-17 23:42:14 +0200

    imp and test jmp

--- nes/main.py
+++ nes/main.py
@@ -3,12 +3,11 @@ from cpu import CPU
 cpu = CPU()
 
 program = [
-    0xA9, 0x01,       # LDA #$01 → N = 0
-    0x10, 0x02,       # BPL +2 (сработает)
-    0xA9, 0xFF,       # LDA #$FF (пропуск)
-    0xA9, 0x05        # LDA #$05
+    0x4C, 0x05, 0x00,  # JMP $0005
+    0xA9, 0x01,        # LDA #$01 (пропуск)
+    0xA9, 0x05,        # LDA #$05
+    0x00               # BRK
 ]
-
 
 cpu.load_program(program)
 
@@ -19,4 +18,3 @@ except StopIteration:
     pass
 
 print(hex(cpu.A))
-print("N =", (cpu.P >> 7) & 1)
