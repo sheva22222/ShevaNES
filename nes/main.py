aea3b3ac8994acf3e81decab703301d09d379b11
whyineedname <emssaoig1@gmail.com>
2026-01-18 17:24:06 +0200

    big test program 2

--- nes/main.py
+++ nes/main.py
@@ -2,44 +2,24 @@ from cpu import CPU
 
 cpu = CPU()
 
-# --------------------
-# программа
-# --------------------
-program = [
-    0xEA,  # NOP
-    0xEA,  # NOP
-    0x00   # BRK (на всякий)
-]
+cpu.PC = 0x8000
+cpu.SP = 0xFD
+cpu.P  = 0b00100000
 
-cpu.load_program(program)
-
-# reset vector → 0x8000
-cpu.memory[0xFFFC] = 0x00
-cpu.memory[0xFFFD] = 0x80
-
-# IRQ vector → 0x9000
+# IRQ vector
 cpu.memory[0xFFFE] = 0x00
 cpu.memory[0xFFFF] = 0x90
-
-cpu.reset()
-
-# Разрешаем IRQ
-cpu.P &= 0b11111011  # CLI вручную (I = 0)
 
 print("PC before IRQ =", hex(cpu.PC))
 print("SP before IRQ =", hex(cpu.SP))
 
-# 🔥 ВЫЗЫВАЕМ IRQ
 cpu.irq()
 
 print("PC after IRQ =", hex(cpu.PC))
 print("SP after IRQ =", hex(cpu.SP))
+
+cpu.step()  # RTI opcode = 0x40
+
+print("PC after RTI =", hex(cpu.PC))
+print("SP after RTI =", hex(cpu.SP))
 print("I =", (cpu.P >> 2) & 1)
-
-# Проверим стек
-p = cpu.memory[0x0100 + cpu.SP + 1]
-low = cpu.memory[0x0100 + cpu.SP + 2]
-high = cpu.memory[0x0100 + cpu.SP + 3]
-
-print("Stack P =", bin(p))
-print("Stack PC =", hex((high << 8) | low))
