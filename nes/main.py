f7468840c0f7f47cbc321725fd7bb4bae05683a5
whyineedname <emssaoig1@gmail.com>
2026-01-18 18:05:27 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -2,26 +2,22 @@ from cpu import CPU
 
 cpu = CPU()
 
-# NMI vector -> 0x9000
-cpu.memory[0xFFFA] = 0x00
-cpu.memory[0xFFFB] = 0x90
-
-cpu.PC = 0x8000
+cpu.P = 0b10000000  # N = 1
 cpu.SP = 0xFD
-cpu.P = 0b00100000  # I = 0
 
-print("PC before NMI =", hex(cpu.PC))
-print("SP before NMI =", hex(cpu.SP))
+cpu.memory[0x8000] = 0x08  # PHP
+cpu.memory[0x8001] = 0x00  # BRK
+cpu.PC = 0x8000
 
-cpu.nmi()
+print("SP before =", hex(cpu.SP))
+print("P before  =", bin(cpu.P))
 
-print("PC after NMI =", hex(cpu.PC))
-print("SP after NMI =", hex(cpu.SP))
-print("I =", (cpu.P >> 2) & 1)
+try:
+    while True:
+        cpu.step()
+except StopIteration:
+    pass
 
-p = cpu.memory[0x0100 + cpu.SP + 1]
-low = cpu.memory[0x0100 + cpu.SP + 2]
-high = cpu.memory[0x0100 + cpu.SP + 3]
-
-print("Stack P =", bin(p))
-print("Stack PC =", hex((high << 8) | low))
+print("SP after  =", hex(cpu.SP))
+print("Stack P  =", bin(cpu.memory[0x0100 + cpu.SP + 1]))
+print("P after  =", bin(cpu.P))
