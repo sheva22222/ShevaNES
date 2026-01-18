4371fb49300bc9b798735b0861b8b5a1ca807763
whyineedname <emssaoig1@gmail.com>
2026-01-18 13:18:53 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -1,19 +1,36 @@
-from cpu import CPU
-
 cpu = CPU()
 
+# main program
 program = [
-    0xA9, 0x42,  # LDA #$42
+    0xA9, 0x01,  # LDA #$01
+    0xEA,        # NOP (если нет — временно убери)
     0x00         # BRK
 ]
 
-cpu.load_program(program, 0x8000)
+# irq handler
+irq_handler = [
+    0xA9, 0x42,  # LDA #$42
+    0x00
+]
 
-# reset vector → $8000
+cpu.load_program(program, 0x8000)
+cpu.load_program(irq_handler, 0x9000)
+
+# reset vector
 cpu.memory[0xFFFC] = 0x00
 cpu.memory[0xFFFD] = 0x80
 
+# irq vector
+cpu.memory[0xFFFE] = 0x00
+cpu.memory[0xFFFF] = 0x90
+
 cpu.reset()
+
+# шаг 1
+cpu.step()
+
+# ВЫЗЫВАЕМ IRQ ВРУЧНУЮ
+cpu.irq()
 
 try:
     while True:
@@ -21,5 +38,4 @@ try:
 except StopIteration:
     pass
 
-print(hex(cpu.A))   # должно быть 0x42
-print("PC =", hex(cpu.PC))
+print(hex(cpu.A))  # ДОЛЖНО БЫТЬ 0x42
