28f6847118d6a05537185017de752b4c66bb2f21
whyineedname <emssaoig1@gmail.com>
2026-01-19 01:12:09 +0200

    Update main.py

--- nes/main.py
+++ nes/main.py
@@ -2,17 +2,16 @@ from cpu import CPU
 
 cpu = CPU()
 
-# Программа:
 # SEC
 # LDA #$05
 # SBC #$03
 # BRK
-cpu.memory[0x8000] = 0x38  # SEC
-cpu.memory[0x8001] = 0xA9  # LDA #
+cpu.memory[0x8000] = 0x38
+cpu.memory[0x8001] = 0xA9
 cpu.memory[0x8002] = 0x05
-cpu.memory[0x8003] = 0xE9  # SBC #
+cpu.memory[0x8003] = 0xE9
 cpu.memory[0x8004] = 0x03
-cpu.memory[0x8005] = 0x00  # BRK
+cpu.memory[0x8005] = 0x00
 
 cpu.PC = 0x8000
 
