2786334843e22e61e2f3360a2beff4e6d412440f
whyineedname <emssaoig1@gmail.com>
2026-01-18 17:39:47 +0200

    4

--- nes/main.py
+++ nes/main.py
@@ -2,27 +2,22 @@ from cpu import CPU
 
 cpu = CPU()
 
-cpu.PC = 0x8000
-cpu.SP = 0xFD
-cpu.P  = 0b00100000
+# программа
+cpu.memory[0x8000] = 0x00  # BRK
 
-# IRQ vector -> 0x9000
+# вектор BRK/IRQ
 cpu.memory[0xFFFE] = 0x00
-cpu.memory[0xFFFF] = 0x90
+cpu.memory[0xFFFF] = 0x90  # PC -> 0x9000
 
-# IRQ handler: RTI
-cpu.memory[0x9000] = 0x40  # RTI
+cpu.PC = 0x8000
 
-print("PC before IRQ =", hex(cpu.PC))
-print("SP before IRQ =", hex(cpu.SP))
+print("PC before =", hex(cpu.PC))
+print("SP before =", hex(cpu.SP))
 
-cpu.irq()
+cpu.step()
 
-print("PC after IRQ =", hex(cpu.PC))
-print("SP after IRQ =", hex(cpu.SP))
+print("PC after =", hex(cpu.PC))
+print("SP after =", hex(cpu.SP))
 
-cpu.step()  # executes RTI
-
-print("PC after RTI =", hex(cpu.PC))
-print("SP after RTI =", hex(cpu.SP))
-print("I =", (cpu.P >> 2) & 1)
+p = cpu.memory[0x0100 + cpu.SP + 1]
+print("Stack P =", bin(p))
