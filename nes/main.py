d21b4a954fb7aae46472615d7218d2317575f92f
whyineedname <emssaoig1@gmail.com>
2026-01-18 13:37:17 +0200

    First try test program

--- nes/main.py
+++ nes/main.py
@@ -2,14 +2,17 @@ from cpu import CPU
 
 cpu = CPU()
 
+# main program
 program = [
-    0xA9, 0x01,
-    0x00
+    0xA9, 0x01,  # LDA #$01
+    0xEA,        # NOP
+    0x00         # BRK
 ]
 
+# irq handler
 irq_handler = [
-    0xA9, 0x42,
-    0x00
+    0xA9, 0x42,  # LDA #$42
+    0x40         # RTI
 ]
 
 cpu.load_program(program, 0x8000)
@@ -23,13 +26,13 @@ cpu.memory[0xFFFF] = 0x90
 
 cpu.reset()
 
-cpu.step()
-cpu.irq()
+cpu.step()   # LDA #$01
+cpu.irq()    # IRQ
 
-try:
-    while True:
-        cpu.step()
-except StopIteration:
-    pass
+cpu.step()   # LDA #$42
+cpu.step()   # RTI
 
-print(hex(cpu.A))
+cpu.step()   # NOP (возврат!)
+
+print(hex(cpu.A))   # должно быть 0x42
+print("PC =", hex(cpu.PC))
