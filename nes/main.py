1bbb7fcd05fadfba4aa87c690b2f646593bb4d66
whyineedname <emssaoig1@gmail.com>
2026-01-18 13:26:24 +0200

    dont test only on cpu.py

--- nes/main.py
+++ nes/main.py
@@ -1,35 +1,29 @@
+from cpu import CPU
+
 cpu = CPU()
 
-# main program
 program = [
-    0xA9, 0x01,  # LDA #$01
-    0xEA,        # NOP (если нет — временно убери)
-    0x00         # BRK
+    0xA9, 0x01,
+    0x00
 ]
 
-# irq handler
 irq_handler = [
-    0xA9, 0x42,  # LDA #$42
+    0xA9, 0x42,
     0x00
 ]
 
 cpu.load_program(program, 0x8000)
 cpu.load_program(irq_handler, 0x9000)
 
-# reset vector
 cpu.memory[0xFFFC] = 0x00
 cpu.memory[0xFFFD] = 0x80
 
-# irq vector
 cpu.memory[0xFFFE] = 0x00
 cpu.memory[0xFFFF] = 0x90
 
 cpu.reset()
 
-# шаг 1
 cpu.step()
-
-# ВЫЗЫВАЕМ IRQ ВРУЧНУЮ
 cpu.irq()
 
 try:
@@ -38,4 +32,4 @@ try:
 except StopIteration:
     pass
 
-print(hex(cpu.A))  # ДОЛЖНО БЫТЬ 0x42
+print(hex(cpu.A))
