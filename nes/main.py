770da0368e8ff319d2b5ba7bf0fca28bc09ce3be
whyineedname <emssaoig1@gmail.com>
2026-01-18 17:28:58 +0200

    3

--- nes/main.py
+++ nes/main.py
@@ -6,9 +6,12 @@ cpu.PC = 0x8000
 cpu.SP = 0xFD
 cpu.P  = 0b00100000
 
-# IRQ vector
+# IRQ vector -> 0x9000
 cpu.memory[0xFFFE] = 0x00
 cpu.memory[0xFFFF] = 0x90
+
+# IRQ handler: RTI
+cpu.memory[0x9000] = 0x40  # RTI
 
 print("PC before IRQ =", hex(cpu.PC))
 print("SP before IRQ =", hex(cpu.SP))
@@ -18,7 +21,7 @@ cpu.irq()
 print("PC after IRQ =", hex(cpu.PC))
 print("SP after IRQ =", hex(cpu.SP))
 
-cpu.step()  # RTI opcode = 0x40
+cpu.step()  # executes RTI
 
 print("PC after RTI =", hex(cpu.PC))
 print("SP after RTI =", hex(cpu.SP))
