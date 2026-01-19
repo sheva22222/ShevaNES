2aa69fd6151517dc6d00bee3e8db667c027e0f43
whyineedname <emssaoig1@gmail.com>
2026-01-19 03:13:05 +0200

    test

--- nes/main.py
+++ nes/main.py
@@ -1,10 +1,10 @@
 from cpu import CPU
 
 cpu = CPU()
-cpu.A = 0b10000001
+cpu.A = 0b00000001
 
-cpu.memory[0x8000] = 0x0A  # ASL A
-cpu.memory[0x8001] = 0x00  # BRK
+cpu.memory[0x8000] = 0x4A
+cpu.memory[0x8001] = 0x00
 cpu.PC = 0x8000
 
 try:
@@ -17,4 +17,3 @@ print("A =", bin(cpu.A))
 print("C =", cpu.P & 1)
 print("Z =", (cpu.P >> 1) & 1)
 print("N =", (cpu.P >> 7) & 1)
-
