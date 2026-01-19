77ae1282b27de6af0d943f40964553dfa7e62c8c
whyineedname <emssaoig1@gmail.com>
2026-01-19 03:37:50 +0200

    test

--- nes/main.py
+++ nes/main.py
@@ -1,11 +1,11 @@
 from cpu import CPU
 
 cpu = CPU()
-cpu.A = 0b00000001
-cpu.P |= 1  # C = 1
+cpu.memory[0x0010] = 0x01
 
-cpu.memory[0x8000] = 0x6A
-cpu.memory[0x8001] = 0x00
+cpu.memory[0x8000] = 0xE6  # INC zp
+cpu.memory[0x8001] = 0x10
+cpu.memory[0x8002] = 0x00  # BRK
 cpu.PC = 0x8000
 
 try:
@@ -14,5 +14,6 @@ try:
 except StopIteration:
     pass
 
-print("A =", bin(cpu.A))
-print("C =", cpu.P & 1)
+print(hex(cpu.memory[0x0010]))
+print("Z =", (cpu.P >> 1) & 1)
+print("N =", (cpu.P >> 7) & 1)
