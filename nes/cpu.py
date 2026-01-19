8766a90760c5418828f1585b4ef041f02f5a49b3
whyineedname <emssaoig1@gmail.com>
2026-01-19 03:47:28 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -467,9 +467,15 @@ class CPU:
             self.A = (self.A >> 1) | (old_c << 7)
             self.update_zn(self.A)
 
-        elif opcode == 0xE6:  # INC zeropage
-            addr = self.fetch_byte()
-            self.INC(addr)
+        elif opcode == 0xE6:  # INC zp
+            addr = self.read(self.PC)
+            self.PC += 1
+
+            value = (self.read(addr) + 1) & 0xFF
+            self.write(addr, value)
+
+            self.set_Z(value)
+            self.set_N(value)
 
         elif opcode == 0xF6:  # INC zeropage,X
             addr = (self.fetch_byte() + self.X) & 0xFF
