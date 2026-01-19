6ed7fd041279153bce0595263adf769aa9edcf07
whyineedname <emssaoig1@gmail.com>
2026-01-19 14:08:54 +0200

    test

--- nes/main.py
+++ nes/main.py
@@ -1,8 +1,7 @@
 from cpu import CPU
 
 cpu = CPU()
-
-cpu.memory[0x0010] = 0x00
+cpu.memory[0x0010] = 0x01
 
 cpu.memory[0x8000] = 0xC6  # DEC zp
 cpu.memory[0x8001] = 0x10
@@ -15,6 +14,6 @@ try:
 except StopIteration:
     pass
 
-print(hex(cpu.memory[0x0010]))
-print("Z =", (cpu.P >> 1) & 1)
-print("N =", (cpu.P >> 7) & 1)
+print(hex(cpu.memory[0x0010]))  # 0x0
+print("Z =", (cpu.P >> 1) & 1)  # 1
+print("N =", (cpu.P >> 7) & 1)  # 0
