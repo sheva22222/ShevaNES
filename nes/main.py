eb84aa1ac5fd24ff44766730488dab45ff23b88b
whyineedname <emssaoig1@gmail.com>
2026-01-19 02:48:24 +0200

    test

--- nes/main.py
+++ nes/main.py
@@ -2,12 +2,12 @@ from cpu import CPU
 
 cpu = CPU()
 
-cpu.A = 0b00000001
-cpu.memory[0x0010] = 0b11000000  # N=1, V=1, A&M = 0
+cpu.A = 0b00001111
 
-cpu.memory[0x8000] = 0x24  # BIT zeropage
-cpu.memory[0x8001] = 0x10
+cpu.memory[0x8000] = 0x29  # AND #imm
+cpu.memory[0x8001] = 0b11110000
 cpu.memory[0x8002] = 0x00  # BRK
+
 cpu.PC = 0x8000
 
 try:
@@ -19,4 +19,3 @@ except StopIteration:
 print("A =", bin(cpu.A))
 print("Z =", (cpu.P >> 1) & 1)
 print("N =", (cpu.P >> 7) & 1)
-print("V =", (cpu.P >> 6) & 1)
