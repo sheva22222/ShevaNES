25aaddbe31e70a9505b8f9fa8cf439ef3f4c72e5
whyineedname <emssaoig1@gmail.com>
2026-01-19 03:20:58 +0200

    impl rol

--- nes/cpu.py
+++ nes/cpu.py
@@ -435,6 +435,18 @@ class CPU:
 
             self.A = (self.A >> 1) & 0xFF
             self.update_zn(self.A)    
+
+        elif opcode == 0x2A:  # ROL A
+            old_c = self.P & 1
+            new_c = (self.A >> 7) & 1
+
+            if new_c:
+                self.P |= 0b00000001
+            else:
+                self.P &= 0b11111110
+
+            self.A = ((self.A << 1) & 0xFF) | old_c
+            self.update_zn(self.A)
         
         else:
             raise Exception(f"Unknown opcode {hex(opcode)}")
