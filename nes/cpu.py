e9225a09cb361ffa16a54ded64086bf811ef3b50
whyineedname <emssaoig1@gmail.com>
2026-01-22 03:12:48 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -2,7 +2,7 @@ class CPU:
     def __init__(self):
         self.Y = 0
         self.I = 0
-        self.P = 0b00100100  # флаг U всегда = 1
+        self.P = 0b00100100  # флаг U всегда = 1овгуовово
         self.A = 0
         self.X = 0
         self.SP = 0xFD
