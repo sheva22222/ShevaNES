8ce9436ee47eb3bfc897ca597b498db7f6f09fe3
whyineedname <emssaoig1@gmail.com>
2026-01-22 03:01:23 +0200

    impl STY 2/2

--- nes/cpu.py
+++ nes/cpu.py
@@ -708,8 +708,9 @@ class CPU:
             self.update_zn(self.A)
 
         elif opcode == 0x84:  # STY zeropage
-            addr = self.fetch_byte()
-            self.memory[addr] = self.AY
+            addr = self.memory[self.PC]
+            self.PC += 1
+            self.memory[addr] = self.Y
 
         elif opcode == 0xF9:  # SBC absolute,Y
             addr = (self.fetch_word() + self.Y) & 0xFFFF
