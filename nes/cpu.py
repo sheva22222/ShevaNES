1b572ae277b31e08795a81c85f208f67e5213e78
whyineedname <emssaoig1@gmail.com>
2026-02-07 01:20:42 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -1015,6 +1015,15 @@ class CPU:
 
             self.X = self.memory[addr]
             self.update_zn(self.X)
+
+        elif opcode == 0x05:  # ORA zeropage
+            addr = self.memory[self.PC]
+            self.PC += 1
+
+            self.A |= self.memory[addr]
+            self.A &= 0xFF
+
+            self.update_zn(self.A)
         
         else:
             raise Exception(f"Unknown opcode {hex(opcode)}")
