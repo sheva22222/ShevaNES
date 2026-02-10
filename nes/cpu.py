10012fadbeda59abe76e5d8d49f5519f1ddbe1d7
whyineedname <emssaoig1@gmail.com>
2026-02-10 18:23:32 +0200

    Update cpu.py

--- nes/cpu.py
+++ nes/cpu.py
@@ -1,13 +1,4 @@
 class CPU:
-    C_FLAG = 0x01  # Carry
-    Z_FLAG = 0x02  # Zero
-    I_FLAG = 0x04  # Interrupt Disable
-    D_FLAG = 0x08  # Decimal
-    B_FLAG = 0x10  # Break
-    U_FLAG = 0x20  # Unused
-    V_FLAG = 0x40  # Overflow
-    N_FLAG = 0x80  # Negative
-    
     FLAG_BITS = {
         'C': 0,
         'Z': 1,
@@ -244,10 +235,12 @@ class CPU:
 
         print(f"PC={pc_before:04X} OP={opcode:02X}")
 
-        if opcode == 0xA9:  # LDA #imm
-            value = self.fetch_byte()
+        if opcode == 0xA9:  # LDA immediate
+            value = self.memory[self.PC]
+            self.PC += 1
             self.A = value
-            self.update_zn(self.A)
+            self.set_flag('Z', self.A == 0)
+            self.set_flag('N', self.A & 0x80)
             
         elif opcode == 0xE8:  # INX
             self.X = (self.X + 1) & 0xFF
@@ -261,12 +254,14 @@ class CPU:
             addr = self.fetch_byte()
             self.memory[addr] = self.X
 
-        elif opcode == 0xF0:
-            offset = self.fetch_byte()
+        elif opcode == 0xF0:  # BEQ
+            offset = self.memory[self.PC]
+            self.PC += 1
+
             if self.get_flag('Z'):
                 if offset & 0x80:
                     offset -= 0x100
-                self.PC = (self.PC + offset) & 0xFFFF
+                self.PC += offset
         
         elif opcode == 0xD0:  # BNE
             offset = self.fetch_byte()
